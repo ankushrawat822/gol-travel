45d24be3c37410e26511e1a42421f4fe6e7bb4b9
ankushrawat822 <ankushrawat037@gmail.com>
2023-01-08 11:34:56 +0530

    navbar updated

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -168,6 +168,9 @@ const [showALmenuMobile, setShowALmenuMobile] = useState(false)
                                 <li onClick={handleALliClickedEventToCloseMobile}><Link className='flex items-center gap-1'
                                     to='/island'> Island
                                 </Link></li>
+                                <li onClick={handleALliClickedEventToCloseMobile}><Link className='flex items-center gap-1'
+                                    to='/gallery'> Gallery
+                                </Link></li>
                 </ul>
             </div>
             }
